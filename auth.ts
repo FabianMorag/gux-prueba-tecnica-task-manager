b47bf3b901fa4d12788717cecd7fe3626f3b50fe
Fabián Morag <fabianmorag0405@gmail.com>
2025-04-28 19:56:07 -0400

    refactor authentication setup and enhance Prisma schema with user and session models

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-// import { prismaClient } from "@/app/lib/prisma";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prismaClient } from "@app/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  // adapter: PrismaAdapter(prismaClient),
+  adapter: PrismaAdapter(prismaClient),
   session: { strategy: "jwt" },
   ...authConfig,
 });
