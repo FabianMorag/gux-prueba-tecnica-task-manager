b6c00d6fe2e58c1b9d770aee276e8c0a0b3f5ce8
Fabián Morag <fabianmorag0405@gmail.com>
2025-04-30 10:10:47 -0400

    Update .dockerignore to include all .env files and modify docker-entrypoint.sh to create a default .env file if it doesn't exist

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,9 @@
+if [ ! -f .env ]; then
+  echo "Creating file .env..."
+  echo "DATABASE_URL=postgresql://postgres:root@postgres_db:5432/prueba_tecnica_gux?schema=public" > .env
+  echo ".env created successfully"
+fi
+
 echo "⏳ Waiting for db..."
 until bunx prisma db push; do
   >&2 echo "❌ Prisma connection to DB failed. Retrying in  2s..."
